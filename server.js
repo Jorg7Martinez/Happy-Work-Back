869cdf17bfda248d4f5a2eb0f6107ada2766af0c
Jorg7Mr <jorgemf195@gmail.com>
2024-11-22 23:28:05 -0300

    cors

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const colors = require("colors"); 
+const colors = require("colors");
 const connectDB = require("./config/mongoose.config");
+const cors = require("cors");
 
 dotenv.config();
 
@@ -9,6 +10,8 @@ dotenv.config();
 connectDB();
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
@@ -24,8 +27,6 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`.yellow.bold);
 });
-
-
 
 /*
 
