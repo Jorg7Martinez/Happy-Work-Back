37718e927661204644d418167da29f4cd00f800b
Luz Lamas <luzlamas604@gmail.com>
2024-11-23 02:13:37 -0300

    comentarios
    comentarios y calificaciones de empresas

--- model/comment.model.js
+++ model/comment.model.js
@@ -1,18 +1,19 @@
 const mongoose = require("mongoose");
 
 const commentSchema = new mongoose.Schema({
-  name: { type: String, default: "Anonimo" },
-  position: { type: String, required: true },
-  company: { type: String, required: true },
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  company: { type: mongoose.Schema.Types.ObjectId, ref: "Company", required: true },
+  name: { type: String, required: true },
+  isAnonymous: { type: Boolean, default: false },
   comment: { type: String, required: true },
   ratings: {
     workLifeBalance: { type: Number, required: true },
     salary: { type: Number, required: true },
     growthOpportunities: { type: Number, required: true },
     workEnvironment: { type: Number, required: true },
-    professionalDevelopment: { type: Number, required: true },
+    professionalDevelopment: { type: Number, required: true }
   },
-  date: { type: Date, default: Date.now },
+  date: { type: Date, default: Date.now }
 });
 
 module.exports = mongoose.model("Comment", commentSchema);
--- routes/comment.routes.js
+++ routes/comment.routes.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const router = express.Router();
-const { addComment, getComments } = require("../controllers/comment.controller");
+const { addComment, getComments,getCommentByCompanyOrId,getAverageRatingsByCompanyId,getOverallAverageRatingByCompanyId } = require("../controllers/comment.controller");
 const { protect } = require("../middlewares/auth.middleware");
 
 router.post("/", protect, addComment);
 router.get("/", getComments);
+router.get("/search", getCommentByCompanyOrId);
+router.get("/average", getAverageRatingsByCompanyId);
+router.get("/overall-average", getOverallAverageRatingByCompanyId);
+
+
+
 
 module.exports = router;
